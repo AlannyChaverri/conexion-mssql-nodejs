bfa7ccfb039f9feac65440db799d8bf2cf40770c
Alanny <achaverrim@est.utn.ac.cr>
2023-01-31 15:47:18 -0600

    1er

---
+++ index.js
@@ -0,0 +1,28 @@
+const sql = require("mssql");
+const { sqlConfig } = require("./database/sql");
+
+const getAllUser = async () => {
+  try {
+    let pool = await sql.connect(sqlConfig);
+    let result = await pool.query`select * from dbo.USERS`;
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const conection = async () => {
+  try {
+    // make sure that any items are correctly URL encoded in the connection string
+    await sql.connect(
+      "Server=PC-ALANNY,1433;Database=Web3;User Id=alanny;Password=root;Encrypt=false"
+    );
+    const result = await sql.query`select * from dbo.USERS`;
+    console.log(result);
+  } catch (err) {
+    // ... error checks
+  }
+};
+
+conection();
+getAllUser();
